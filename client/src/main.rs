fdaf554af91618bd61868724103f2c75660ba0f7
Jasper Lievisse Adriaanse <j@jasper.la>
2020-01-24 21:28:46 +0100

    First parts that resemble a UI
    This still needs to be wired up to use the provided channels to communicate
    with the remote ICB server

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,13 +1,42 @@
-use icb::{packets, Config};
+#[allow(dead_code)]
+mod util;
+use util::{Event, Events};
 
 #[macro_use]
 extern crate clap;
-use clap::App;
 use chrono::{Local, Timelike};
+use clap::App;
 use crossbeam_utils::thread;
+use icb::{packets, Config};
+use std::io::{self, Write};
 use std::time::Duration;
+use termion::cursor::Goto;
+use termion::event::Key;
+use termion::input::MouseTerminal;
+use termion::raw::IntoRawMode;
+use termion::screen::AlternateScreen;
+use tui::backend::TermionBackend;
+use tui::layout::{Constraint, Direction, Layout};
+use tui::style::{Color, Style};
+use tui::widgets::{Block, Borders, List, Paragraph, Text, Widget};
+use tui::Terminal;
+use unicode_width::UnicodeWidthStr;
 
-fn main() {
+struct Ui {
+    input: String,
+    history: Vec<String>,
+}
+
+impl Default for Ui {
+    fn default() -> Ui {
+        Ui {
+            input: String::new(),
+            history: Vec::new(),
+        }
+    }
+}
+
+fn main() -> Result<(), failure::Error> {
     let clap_yaml = load_yaml!("clap.yml");
     let matches = App::from_yaml(clap_yaml).get_matches();
 
@@ -22,6 +51,19 @@ fn main() {
     };
 
     let (client, mut server) = icb::init(config).unwrap();
+
+    // Configure the terminal...
+    let stdout = io::stdout().into_raw_mode()?;
+    let stdout = MouseTerminal::from(stdout);
+    let stdout = AlternateScreen::from(stdout);
+    let backend = TermionBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    // ...and event handlers...
+    let events = Events::new();
+
+    // ...and finally create the default UI state
+    let mut ui = Ui::default();
 
     thread::scope(|s| {
         s.spawn(|_| {
@@ -54,6 +96,84 @@ fn main() {
 
             std::thread::sleep(Duration::from_millis(1));
         });
+
+        loop {
+            terminal
+                .draw(|mut f| {
+                    let chunks = Layout::default()
+                        .direction(Direction::Vertical)
+                        .horizontal_margin(1)
+                        .constraints(
+                            [
+                                Constraint::Length(1),
+                                Constraint::Min(1),
+                                Constraint::Length(3),
+                            ]
+                            .as_ref(),
+                        )
+                        .split(f.size());
+
+                    let help_message = format!("Group: 1, Topic: carrots and peas");
+                    Paragraph::new([Text::raw(help_message)].iter()).render(&mut f, chunks[0]);
+                    Paragraph::new([Text::raw(&ui.input)].iter())
+                        .style(Style::default().fg(Color::Yellow))
+                        .block(Block::default().borders(Borders::TOP))
+                        .render(&mut f, chunks[2]);
+                    let history = ui
+                        .history
+                        .iter()
+                        .enumerate()
+                        .map(|(i, m)| Text::raw(format!("{}: {}", i, m)));
+                    List::new(history)
+                        .block(Block::default().borders(Borders::TOP))
+                        .render(&mut f, chunks[1]);
+                })
+                .expect("Failed to draw UI to terminal");
+
+            let termsize = terminal.size().unwrap();
+            // Put the cursor back inside the input box
+            write!(
+                terminal.backend_mut(),
+                "{}",
+                Goto(2 + ui.input.width() as u16, termsize.height - 1)
+            )
+            .expect("Failed to position cursor");
+            io::stdout().flush().ok();
+
+            // Now read the user input, these could be control actions such as backspace,
+            // commands (starting with '/') or actual messages intended for other users.
+            match events.next().expect("Failed to read user input") {
+                Event::Input(input) => match input {
+                    Key::Backspace => {
+                        ui.input.pop();
+                    }
+                    Key::Ctrl(c) => match c {
+                        'w' => ui.input.clear(),
+                        'a' => {} // XXX move cursor to beginning
+                        'e' => {} // XXX move cursor to end.
+                        _ => {}
+                    },
+                    Key::Char('\n') => {
+                        match ui.input.chars().next() {
+                            Some(v) if v == '/' => {
+                                if ui.input == "/quit" {
+                                    break;
+                                }
+                                // XXX: Handle other commands here
+                            }
+                            _ => ui.history.push(ui.input.drain(..).collect()),
+                        }
+                    }
+                    Key::Char(c) => {
+                        ui.input.push(c);
+                    }
+                    _ => {}
+                },
+                _ => {}
+            }
+        }
     })
     .unwrap();
+
+    Ok(())
 }
