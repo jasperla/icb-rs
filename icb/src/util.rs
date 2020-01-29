e625a2eec27851f1dc4f8373f2ff44119e4664e5
Jasper Lievisse Adriaanse <j@jasper.la>
2020-01-29 21:39:43 +0100

    neuter q() in release builds

--- icb/src/util.rs
+++ icb/src/util.rs
@@ -1,3 +1,4 @@
+#[allow(unused_imports)]
 use std::fs::File;
 use std::io::prelude::*;
 
@@ -6,8 +7,16 @@ use std::io::prelude::*;
 /// For example:
 ///     let vector = vec![1, 2, 3];
 ///     q("v", &vector);
+///
+/// This function is only enabled in debug builds.
+#[cfg(debug_assertions)]
 pub fn q<T: std::fmt::Debug>(msg: &str, thing: &T) -> std::io::Result<()> {
     let mut file = File::create("/tmp/q.log")?;
     file.write_all(format!("{}: {:?}\n", msg, thing).as_bytes())?;
+    Ok(())
+}
+
+#[cfg(not(debug_assertions))]
+pub fn q<T: std::fmt::Debug>(_msg: &str, _thing: &T) -> std::io::Result<()> {
     Ok(())
 }
