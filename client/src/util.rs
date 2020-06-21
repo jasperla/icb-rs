564524911c6d339913f29826a3c3b432847b2703
Todd Mortimer <todd@opennet.ca>
2020-06-21 17:52:16 -0400

    Refactor main loop to improve responsiveness.
    - Eliminate Event::Tick in favor of polling for input
    - Read multiple key events in a single pass
    - Only redraw screen if something has changed
    
    With these changes pasted input now appears immediately.

--- client/src/util.rs
+++ client/src/util.rs
@@ -3,43 +3,22 @@
 use std::io;
 use std::sync::mpsc;
 use std::thread;
-use std::time::Duration;
 
 use termion::event::Key;
 use termion::input::TermRead;
 
 pub enum Event<I> {
     Input(I),
-    Tick,
 }
 
-/// A small event handler that wrap termion input and tick events. Each event
-/// type is handled in its own thread and returned to a common `Receiver`
+/// A small event handler that wrap termion input events.
 pub struct Events {
     rx: mpsc::Receiver<Event<Key>>,
     input_handle: thread::JoinHandle<()>,
-    tick_handle: thread::JoinHandle<()>,
-}
-
-#[derive(Debug, Clone, Copy)]
-pub struct Config {
-    pub tick_rate: Duration,
-}
-
-impl Default for Config {
-    fn default() -> Config {
-        Config {
-            tick_rate: Duration::from_millis(250),
-        }
-    }
 }
 
 impl Events {
     pub fn new() -> Events {
-        Events::with_config(Config::default())
-    }
-
-    pub fn with_config(config: Config) -> Events {
         let (tx, rx) = mpsc::channel();
         let input_handle = {
             let tx = tx.clone();
@@ -54,23 +33,10 @@ impl Events {
                 }
             })
         };
-        let tick_handle = {
-            thread::spawn(move || {
-                let tx = tx.clone();
-                loop {
-                    tx.send(Event::Tick).unwrap();
-                    thread::sleep(config.tick_rate);
-                }
-            })
-        };
-        Events {
-            rx,
-            input_handle,
-            tick_handle,
-        }
+        Events { rx, input_handle }
     }
 
-    pub fn next(&self) -> Result<Event<Key>, mpsc::RecvError> {
-        self.rx.recv()
+    pub fn next(&self) -> Result<Event<Key>, mpsc::TryRecvError> {
+        self.rx.try_recv()
     }
 }
